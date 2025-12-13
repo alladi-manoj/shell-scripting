6b3dd6850cfeff3feac5509f6957652339fae42e
alladi-manoj <manojbenahr.alladi@gmail.com>
2025-12-13 11:22:23 +0530

    practice

---
+++ variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1=ramesh
+person2=suresh
+
+
+echo "$person1: hello $person2, how are you?"
+echo "$person2: hi $person1, i am fine, thank you!"
+echo "$person1: that's great to hear!"
+echo "$person2: what about you?"
+echo "$person1: i am doing well too."
