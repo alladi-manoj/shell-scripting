04d577589c909ef07e069331466ff3d6d14c05ed
alladi-manoj <manojbenahr.alladi@gmail.com>
2025-12-13 11:15:02 +0530

    practice

--- hello-world2.sh
+++ hello-world2.sh
@@ -1,2 +1,7 @@
-#this is hello world sample1
-want to save
+#!/bin/bash
+
+echo "ramesh: hello suresh, how are you?"
+echo "suresh: hi ramesh, i am fine, thank you!"
+echo "ramesh: that's great to hear!"
+echo "suresh: what about you?"
+echo "ramesh: i am doing well too."
