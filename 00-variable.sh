949d5e55333799c62df2b28f978be1dd6a60826f
alladi-manoj <manojbenahr.alladi@gmail.com>
2026-01-30 16:08:09 +0530

    some-msg

--- 00-variable.sh
+++ 00-variable.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+ 
+#declaring variables
 person1=rahul
 person2=sachin
 
-
+#using variables
 echo "$person1: hello $person2, how are you?"
 echo "$person2: hi $person1, i am fine, thank you!"
 echo "$person1: that's great to hear!"
